aeae3f33e3744d160ad2c43cddc8e7ef18841b2c
Alexandre Morgaut <49318+AMorgaut@users.noreply.github.com>
2024-06-13 22:40:50 +0200

    feat: show the ABCD widget in the App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import AbcdeScore from './components/AbcdeScore.vue'
 </script>
 
 <template>
@@ -8,7 +8,8 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      Example de widget
+      <AbcdeScore value="C" severity="Medium" />
     </div>
   </header>
 
