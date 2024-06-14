a8217180fe4256a5f27b19dd219d3e57ecb4286c
Alexandre Morgaut <49318+AMorgaut@users.noreply.github.com>
2024-06-14 16:51:29 +0200

    feat: add storybook accessibility addons

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,17 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: [
-      '@storybook/addon-links',
-      '@storybook/addon-essentials',
-      '@chromatic-com/storybook',
-      '@storybook/addon-interactions',
-      '@storybook/addon-a11y'
-    ],
-    framework: {
-      name: '@storybook/vue3-vite',
-      options: {}
-    }
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    'addon-screen-reader',
+    '@storybook/addon-a11y'
+  ],
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {}
   }
-  export default config
+}
+export default config
