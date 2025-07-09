6b86e009181fa929665e50409a4c34abcaec773c
Alexandre Morgaut <alexandre.morgaut@capgemini.com>
2025-07-09 14:46:58 +0200

    fix: update msw handler to handle measure API

--- src/api/sonar/mock/msw-handler.js
+++ src/api/sonar/mock/msw-handler.js
@@ -3,7 +3,7 @@ import { http, HttpResponse } from 'msw'
 export const handlers = [
   http.get(`/api/:api/:route`, async ({ params }) => {
     const { api, route } = params
-    const { default: body } = await import(`../issues/sonar.${api}.${route}.mock.js`)
+    const { default: body } = await import(`../${api}/sonar.${api}.${route}.mock.js`)
     return HttpResponse.json(body)
   }),
 ]
